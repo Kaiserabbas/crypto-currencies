e2486dfc38fc55538ec490a9931d84fc2466c885
Qaisar <kayser.abbas@gmail.com>
2023-09-10 13:15:12 +0500

    Modify reducers

--- src/reducers/airPollutionReducer.js
+++ src/reducers/airPollutionReducer.js
@@ -8,9 +8,13 @@ const initialState = {
 const airPollutionReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_AIR_POLLUTION_SUCCESS':
-      return { ...state, data: action.payload, loading: false, error: null };
+      return {
+        ...state, data: action.payload, loading: false, error: null,
+      };
     case 'FETCH_AIR_POLLUTION_FAILURE':
-      return { ...state, data: null, loading: false, error: action.payload };
+      return {
+        ...state, data: null, loading: false, error: action.payload,
+      };
     default:
       return state;
   }
