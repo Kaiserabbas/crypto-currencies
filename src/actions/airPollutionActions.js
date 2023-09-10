f61bf8a1fe7850e702d9f5e2f9a848984cce8f4f
Qaisar <kayser.abbas@gmail.com>
2023-09-10 13:15:30 +0500

    Modify Actions

--- src/actions/airPollutionActions.js
+++ src/actions/airPollutionActions.js
@@ -1,9 +1,10 @@
 // actions/airPollutionActions.js
 import axios from 'axios';
 
-const API_KEY = '0fd89f3c751f217b80034bb11909263a'; 
+const API_KEY = '0fd89f3c751f217b80034bb11909263a';
+const API_URL = 'https://api.openweathermap.org/data/2.5/air_pollution';
 
-export const fetchAirPollution = (lat, lon) => async (dispatch) => {
+const fetchAirPollution = (lat, lon) => async (dispatch) => {
   try {
     const response = await axios.get(`${API_URL}?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
     dispatch({ type: 'FETCH_AIR_POLLUTION_SUCCESS', payload: response.data });
@@ -11,4 +12,5 @@ export const fetchAirPollution = (lat, lon) => async (dispatch) => {
     dispatch({ type: 'FETCH_AIR_POLLUTION_FAILURE', payload: error });
   }
 };
-    
+
+export default fetchAirPollution;
