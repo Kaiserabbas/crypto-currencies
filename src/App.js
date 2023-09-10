7cc0e9a845b14c1cd43409b892c26c548ab23b9b
Qaisar <kayser.abbas@gmail.com>
2023-09-10 13:16:13 +0500

    Modify App.js for the Project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Home from './Home';
+import DetailPage from './DetailPage';
+import store from './redux/store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/detail/:cityId" component={DetailPage} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
