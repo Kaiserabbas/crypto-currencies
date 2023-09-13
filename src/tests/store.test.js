9c0f08d09ec45dccc5ca81a7a105a517eb6d8ef7
Qaisar <kayser.abbas@gmail.com>
2023-09-13 18:51:34 +0500

    Add test for store.js

---
+++ src/tests/store.test.js
@@ -0,0 +1,30 @@
+import { configureStore } from '@reduxjs/toolkit';
+import store from '../redux/store'; 
+import cryptoReducer from '../redux/reducers/cryptoReducer';
+describe('Redux Store', () => {
+  let testStore; 
+
+ 
+  beforeEach(() => {
+    testStore = configureStore({
+      reducer: {
+        coins: cryptoReducer,
+      },
+    });
+  });
+
+  // Test that the store configuration is correct
+  it('should configure the store with the cryptoReducer', () => {
+    const expectedInitialState = {
+      coins: {
+        cryptos: [],
+      },
+    };
+    expect(testStore.getState()).toEqual(expectedInitialState);
+  });
+it('should export the configured store', () => {
+  const storeState = store.getState();
+  const testStoreState = testStore.getState();
+  expect(testStoreState).toEqual(storeState);
+});
+});
